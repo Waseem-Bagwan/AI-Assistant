68686e94b6123b0a644b61f41ec05236c28afc6e
Waseem Bagwan <waseembagwan950@gmail.com>
2026-04-02 19:47:46 +0530

    added the endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,11 @@ import aiService from './routes/review.route.js'
 const app = express()
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:[
+        "http://localhost:5173",
+        "https://ai-assistant-beta-flax.vercel.app/"
+    ],
+    credentials: true
 }))
 
 app.use(express.json())
