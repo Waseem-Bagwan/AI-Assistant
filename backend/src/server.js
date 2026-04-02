3922a4e0406481a23bab837af094d1190fd6d268
Waseem Bagwan <waseembagwan950@gmail.com>
2026-04-02 21:19:55 +0530

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-import path from 'path'
-import config from './config/config.js'
-import aiService from './routes/review.route.js'
+    import express from 'express'
+    import cors from 'cors'
+    import config from './config/config.js'
+    import aiService from './routes/review.route.js'
 
-const app = express()
+    const app = express()
 
-app.use(cors({
-    origin:[
-        "http://localhost:5173",
-        "https://ai-assistant-beta-flax.vercel.app"
-    ],
-    credentials: true
-}))
+    app.use(cors({
+        origin:[
+            "http://localhost:5173",
+            "https://ai-assistant-beta-flax.vercel.app"
+        ],
+        credentials: true
+    }))
 
-app.use(express.json())
+    app.use(express.json())
 
-app.use('/api/v1/reviews',aiService)
+    app.use('/api/v1/reviews',aiService)
 
 
-app.listen(config.PORT,() => {
-    console.log(`Server up and running on PORT: ${config.PORT}`)
-})
+    app.listen(config.PORT,() => {
+        console.log(`Server up and running on PORT: ${config.PORT}`)
+    })
