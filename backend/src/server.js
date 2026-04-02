974c0bdd13d776ae0168b41438a9ca965699349b
Waseem Bagwan <waseembagwan950@gmail.com>
2026-04-02 19:17:47 +0530

    separating deployment process

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,6 @@ import config from './config/config.js'
 import aiService from './routes/review.route.js'
 
 const app = express()
-const __dirname = path.resolve() 
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -15,12 +14,6 @@ app.use(express.json())
 
 app.use('/api/v1/reviews',aiService)
 
-if(config.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"))
-    })
-}
 
 app.listen(config.PORT,() => {
     console.log(`Server up and running on PORT: ${config.PORT}`)
